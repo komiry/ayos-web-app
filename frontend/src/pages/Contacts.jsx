54dd3bffb47fb5c4ffe9cdc5f9485e6a49ab4e51
MartinMichaelVincent <145082718+MartinMichaelVincent@users.noreply.github.com>
2025-01-06 05:45:54 +0000

    pages

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
-const Contacts = () => {
+const Contact = () => {
   return (
     <div>
-      
+
+      <div className='text-center text-2xl pt-10 text-[#707070]'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className=' text-gray-500'>San Marcelino St, Ayala Blvd<br /> Ermita, Manila, PH</p>
+          <p className=' text-gray-500'>Tel: 915 909 2293 <br /> Email: ryagondev@gmail.com</p>
+          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT AYOS</p>
+          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+
     </div>
   )
 }
 
-export default Contacts
+export default Contact
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopTechnicians from '../components/TopTechnicians'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
-    Nice once
-    
+      <Header />
+      <SpecialityMenu />
+      <TopTechnicians />
+      <Banner />
     </div>
   )
 }
