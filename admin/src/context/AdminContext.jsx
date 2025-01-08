da3247fbf282b3d3b29526618622e62129931c32
marc00000s <marcosdarwin.tulalian@tup.edu.ph>
2025-01-08 14:56:34 +0800

    context

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -32,6 +32,99 @@ const AdminContextProvider = (props) => {
 
     }
 
+    const changeAvailability = async (tcId) => {
+        try {
 
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availability', { tcId }, { headers: { aToken } })
+            if (data.success) {
+                toast.success(data.message)
+                getAlltechs()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+
+    const getAllAppointments = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/appointments', { headers: { aToken } })
+            if (data.success) {
+                setAppointments(data.appointments.reverse())
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+
+    }
+
+    // Function to cancel appointment using API
+    const cancelAppointment = async (appointmentId) => {
+
+        try {
+
+            const { data } = await axios.post(backendUrl + '/api/admin/cancel-appointment', { appointmentId }, { headers: { aToken } })
+
+            if (data.success) {
+                toast.success(data.message)
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+
+    }
+
+    const getDashData = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } })
+
+            if (data.success) {
+                setDashData(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
+    const value = {
+        aToken, setAToken,
+        techs,
+        getAllTechs,
+        changeAvailability,
+        appointments,
+        getAllAppointments,
+        getDashData,
+        cancelAppointment,
+        dashData
+    }
+
+    return (
+        <AdminContext.Provider value={value}>
+            {props.children}
+        </AdminContext.Provider>
+    )
+
+}
 
 export default AdminContextProvider
