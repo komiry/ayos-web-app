b029313c34f1a4fceab63a86872eb7d18713e550
unknown <mm7211827@gmail.com>
2025-01-21 19:01:06 +0800

    Fixed: missing assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,77 @@
+import appointment_img from './appointment_img.png'
+import header_img from './header_img.png'
+import group_profiles from './group_profiles.png'
+import profile_pic from './profile_pic.png'
+import contact_image from './contact_image.png'
+import about_image from './about_image.png'
+import logo from './logo.svg'
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.png'
+import chats_icon from './chats_icon.svg'
+import verified_icon from './verified_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import info_icon from './info_icon.svg'
+import upload_icon from './upload_icon.png'
+import stripe_logo from './stripe_logo.png'
+import razorpay_logo from './razorpay_logo.png'
+import Aircon_Repair from './Aircon_Repair.svg'
+import Electrician from './Electrician.svg'
+import Phone_Technician from './Phone_Technician.svg'
+import Plumber from './Plumber.svg'
+import Painter from './Painter.svg'
+import Carpenter from './Carpenter.svg'
+
+
+export const assets = {
+    appointment_img,
+    header_img,
+    group_profiles,
+    logo,
+    chats_icon,
+    verified_icon,
+    info_icon,
+    profile_pic,
+    arrow_icon,
+    contact_image,
+    about_image,
+    menu_icon,
+    cross_icon,
+    dropdown_icon,
+    upload_icon,
+    stripe_logo,
+    razorpay_logo
+}
+
+export const specialityData = [
+    {
+        speciality: 'Phone Technician',
+        image: Phone_Technician
+    },
+    {
+        speciality: 'Plumber',
+        image: Plumber
+    },
+    {
+        speciality: 'Aircon Repair',
+        image: Aircon_Repair
+    },
+    {
+        speciality: 'Carpenter',
+        image: Carpenter
+    },
+    {
+        speciality: 'Painter',
+        image: Painter
+    },
+    {
+        speciality: 'Electrician',
+        image: Electrician
+    },
+]
+
+export const technician = [
+    {
+        
+    },
+]
