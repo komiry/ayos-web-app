a12ba69fdee4d4aeacc6dbaabf2087a0ad2bbb77
unknown <mm7211827@gmail.com>
2025-01-21 19:19:05 +0800

    Fixed: technician

--- frontend/src/components/TopTechnician.jsx
+++ frontend/src/components/TopTechnician.jsx
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
-const TopTechnicians = () => {
+import { technician } from '../assets/assets'
+
+const TopTechnician = () => {
 
     const navigate = useNavigate()
 
-    const { technicians } = useContext(AppContext)
+    const { technician } = useContext(AppContext)
 
     return (
         <div className='flex flex-col items-center gap-4 my-16 text-[#262626] md:mx-10'>
             <h1 className='text-3xl font-medium'>Top Technician to Book</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trained technician.</p>
             <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-                {technicians.slice(0, 10).map((item, index) => (
+                {technician.slice(0, 10).map((item, index) => (
                     <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-[#C9D8FF] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                         <img className='bg-[#EAEFFF]' src={item.image} alt="" />
                         <div className='p-4'>
@@ -20,15 +22,14 @@ const TopTechnicians = () => {
                                 <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : "bg-gray-500"}`}></p><p>{item.available ? 'Available' : "Not Available"}</p>
                             </div>
                             <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
-                            <p className='text-[#5C5C5C] text-sm'>{item.speciality}</p>
                         </div>
                     </div>
                 ))}
             </div>
-            <button onClick={() => { navigate('/technicians'); scrollTo(0, 0) }} className='bg-[#EAEFFF] text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+            <button onClick={() => { navigate('/technician'); scrollTo(0, 0) }} className='bg-[#EAEFFF] text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
         </div>
 
     )
 }
 
-export default TopTechnicians
+export default TopTechnician
